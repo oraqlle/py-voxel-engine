1475663682f9184e95307c330414e5a3c2e0b447
oraqlle <41113853+oraqlle@users.noreply.github.com>
2025-06-22 21:46:33 +1000

    wip: Made terrain gen appear more natural

--- terrain_gen.py
+++ terrain_gen.py
@@ -8,11 +8,21 @@ from numba import njit
 def get_height(x, z):
     # amplitude
     a1 = CENTER_Y
+    a2, a4, a8 = a1 * 0.5, a1 * 0.25, a1 * 0.125
 
     # frequency
     f1 = 0.005
+    f2, f4, f8 = f1 * 2, f1 * 4, f1 * 8
+
+    if noise2(0.1 * x, 0.1 * z) < 0:
+        a1 /= 1.07
 
     height = 0
     height += noise2(x * f1, z * f1) * a1 + a1
+    height += noise2(x * f2, z * f2) * a2 - a2
+    height += noise2(x * f4, z * f4) * a4 + a4
+    height += noise2(x * f8, z * f8) * a8 - a8
+
+    height = max(height, 1)
 
     return int(height)
